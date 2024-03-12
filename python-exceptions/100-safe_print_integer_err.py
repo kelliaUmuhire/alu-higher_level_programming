0aeb80d892a8eda27eba4b73ce9bcb3a43a1bc54
kelliaUmuhire <kellumuhire@gmail.com>
2024-03-12 08:13:09 -0700

    Checl

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as error:
-        import sys
-        print("Exception: {}".format(error), file=sys.stderr)
-        return False
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return (False)
     else:
-        return True
+        return (True)
